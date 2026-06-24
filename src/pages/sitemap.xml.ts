d3d06bf0509b30a04840657dfb9a4e6f490eca3b
ajing <ajingnk@gmail.com>
2026-06-24 11:00:23 -0700

    Fix Search Console sitemap discovery

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,16 +1,63 @@
 import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import { SITE } from "@/config";
+import { TOPICS } from "@/data/topics";
+import { getPath } from "@/utils/getPath";
+import getSortedPosts from "@/utils/getSortedPosts";
 
-export const GET: APIRoute = ({ site }) => {
-  const sitemapURLs = ["sitemap-0.xml", "discovery-sitemap.xml"].map(path =>
-    new URL(path, site)
+const staticPages = [
+  "/",
+  "/about/",
+  "/archives/",
+  "/posts/",
+  "/start-here/",
+  "/topics/",
+] as const;
+
+const escapeXml = (value: string) =>
+  value
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+
+const urlEntry = ({ loc, lastmod }: { loc: string; lastmod?: string }) =>
+  [
+    "  <url>",
+    `    <loc>${escapeXml(loc)}</loc>`,
+    lastmod && `    <lastmod>${lastmod}</lastmod>`,
+    "  </url>",
+  ]
+    .filter(Boolean)
+    .join("\n");
+
+export const GET: APIRoute = async ({ site }) => {
+  const origin = site ?? new URL(SITE.website);
+  const posts = getSortedPosts(await getCollection("blog"));
+  const urls = [
+    ...staticPages.map((path) => ({ loc: new URL(path, origin).href })),
+    ...TOPICS.map((topic) => ({
+      loc: new URL(`/topics/${topic.slug}/`, origin).href,
+    })),
+    ...posts.map((post) => ({
+      loc: new URL(`${getPath(post.id, post.filePath)}/`, origin).href,
+      lastmod: (post.data.modDatetime ?? post.data.pubDatetime)
+        .toISOString()
+        .slice(0, 10),
+    })),
+  ];
+
+  const dedupedUrls = Array.from(
+    new Map(urls.map((url) => [url.loc, url])).values(),
   );
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${sitemapURLs
-    .map(url => `  <sitemap>\n    <loc>${url.href}</loc>\n  </sitemap>`)
-    .join("\n")}\n</sitemapindex>\n`;
+
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${dedupedUrls
+    .map(urlEntry)
+    .join("\n")}\n</urlset>\n`;
 
   return new Response(body, {
     headers: {
-      "Content-Type": "application/xml",
+      "Content-Type": "application/xml; charset=utf-8",
     },
   });
 };
