16c4df32a2b2a4e2ca8566b81ebb883b3fe45b99
ajing <ajingnk@gmail.com>
2026-06-24 11:01:02 -0700

    Point robots at canonical sitemap

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -4,11 +4,10 @@ const getRobotsTxt = (sitemapURL: URL) => `
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL("sitemap.xml", sitemapURL).href}
 Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap-index.xml", site);
+  const sitemapURL = new URL("sitemap.xml", site);
   return new Response(getRobotsTxt(sitemapURL));
 };
