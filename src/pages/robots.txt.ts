fcb0c55c6f415370f27224bf10154e10eafca1a3
ajing <ajingnk@gmail.com>
2026-06-24 17:05:05 -0700

    Advertise plain text sitemap

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,13 +1,19 @@
 import type { APIRoute } from "astro";
+import { SITE } from "@/config";
 
-const getRobotsTxt = (sitemapURL: URL) => `
+const getRobotsTxt = (sitemapURLs: URL[]) => `
 User-agent: *
 Allow: /
 
-Sitemap: ${sitemapURL.href}
+${sitemapURLs.map((sitemapURL) => `Sitemap: ${sitemapURL.href}`).join("\n")}
 `;
 
 export const GET: APIRoute = ({ site }) => {
-  const sitemapURL = new URL("sitemap.xml", site);
-  return new Response(getRobotsTxt(sitemapURL));
+  const origin = site ?? new URL(SITE.website);
+  return new Response(
+    getRobotsTxt([
+      new URL("sitemap.xml", origin),
+      new URL("sitemap.txt", origin),
+    ]),
+  );
 };
