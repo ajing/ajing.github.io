1fa3f817f53b7e774870857402c444b11ac7cca4
ajing <ajingnk@gmail.com>
2026-06-24 17:05:17 -0700

    Share sitemap URL generation

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,18 +1,5 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
-import { SITE } from "@/config";
-import { TOPICS } from "@/data/topics";
-import { getPath } from "@/utils/getPath";
-import getSortedPosts from "@/utils/getSortedPosts";
-
-const staticPages = [
-  "/",
-  "/about/",
-  "/archives/",
-  "/posts/",
-  "/start-here/",
-  "/topics/",
-] as const;
+import { getCanonicalSitemapEntries } from "@/utils/sitemapUrls";
 
 const escapeXml = (value: string) =>
   value
@@ -32,26 +19,9 @@ const urlEntry = ({ loc, lastmod }: { loc: string; lastmod?: string }) =>
     .join("\n");
 
 export const GET: APIRoute = async ({ site }) => {
-  const origin = site ?? new URL(SITE.website);
-  const posts = getSortedPosts(await getCollection("blog"));
-  const urls = [
-    ...staticPages.map((path) => ({ loc: new URL(path, origin).href })),
-    ...TOPICS.map((topic) => ({
-      loc: new URL(`/topics/${topic.slug}/`, origin).href,
-    })),
-    ...posts.map((post) => ({
-      loc: new URL(`${getPath(post.id, post.filePath)}/`, origin).href,
-      lastmod: (post.data.modDatetime ?? post.data.pubDatetime)
-        .toISOString()
-        .slice(0, 10),
-    })),
-  ];
+  const urls = await getCanonicalSitemapEntries(site);
 
-  const dedupedUrls = Array.from(
-    new Map(urls.map((url) => [url.loc, url])).values(),
-  );
-
-  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${dedupedUrls
+  const body = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls
     .map(urlEntry)
     .join("\n")}\n</urlset>\n`;
 
